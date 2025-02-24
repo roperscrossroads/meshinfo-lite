d0e28da2af9f60b98ec7826ed16044adf8ec21b9
Johannes le Roux <dade@dade.co.za>
2025-02-25 01:47:27 +0200

    added streak

--- meshtastic_monday.py
+++ meshtastic_monday.py
@@ -1,4 +1,6 @@
 from time import strftime, localtime
+from datetime import datetime, timedelta
+import json
 
 
 class MeshtasticMonday:
@@ -21,33 +23,52 @@ class MeshtasticMonday:
                 current = frm + "." + text
                 if current == uniq:
                     continue
-                dt = strftime('%a %b %d %Y', localtime(ts))
+                dt = str(datetime.fromtimestamp(ts).date())
                 tmp = dict(message)
                 tmp["monday"] = dt
                 monday.append(tmp)
                 uniq = current
         monday = sorted(monday, key=lambda x: x['timestamp'])
-        monday.reverse()
         self.monday = monday
 
     def check_ins(self):
         nodes = {}
+        if not self.monday:
+            return nodes
         for monday in self.monday:
             frm = monday["from"]
             if frm not in nodes:
                 nodes[frm] = {
                     "check_ins": 0,
-                    "streak": 0,
+                    "streak": 1,
                     "mondays": []
                 }
             dt = monday["monday"]
-            if dt not in nodes[frm]["mondays"]:
+            mondays = nodes[frm]["mondays"]
+            if dt not in mondays:
                 nodes[frm]["mondays"].append(dt)
                 nodes[frm]["check_ins"] += 1
+
+        #  Calculate streak
+        latest_monday = self.monday[-1]["monday"]
+        for node in nodes:
+            mondays = list(nodes[node]["mondays"])
+            if latest_monday not in mondays:
+                nodes[node]["streak"] = 0
+                continue
+            for monday in mondays:
+                dto = datetime.strptime(monday, '%Y-%m-%d')
+                lastweek = str((dto.date() - timedelta(days=7)))
+                if lastweek in mondays:
+                    nodes[node]["streak"] += 1
+                else:
+                    nodes[node]["streak"] = 0
         return nodes
 
     def get_data(self):
+        rev = list(self.monday)
+        rev.reverse()
         return {
-            "messages": self.monday,
+            "messages": rev,
             "nodes": self.check_ins()
         }
